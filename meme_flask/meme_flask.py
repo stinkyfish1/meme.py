fe0e00c2ff4ac84a17b035ecc5ed06cc2bffefbd
nikki <156199864+stinkyfish1@users.noreply.github.com>
2024-09-26 13:29:04 +0200

    Update meme_flask.py

--- meme_flask/meme_flask.py
+++ meme_flask/meme_flask.py
@@ -5,9 +5,6 @@ import json
 app = Flask(__name__)
 
 def get_meme():
-    #Uncomment these two lines and comment out the other url line if you want to use a specific meme subreddt
-    # sr = "/wholesomememes"
-    # url = "<https://meme-api.herokuapp.com/gimme>" + sr
     url = "<https://meme-api.com/gimme>"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
